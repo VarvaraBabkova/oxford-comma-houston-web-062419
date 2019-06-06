6d833352ae2513fd63b0b6530b939a5a3c13f87c
Varvara Babkova <varvara.munday@gmail.com>
2019-06-06 23:20:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   end
   ar1 = array.dup
   ar1.pop
-  return ar1.join(", ") << "and " + array[array.size - 1]
+  return ar1.join(", ") << " and " + array[array.size - 1]
 
 end
